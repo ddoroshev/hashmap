dcc829660d855d4d994a2ca24861001665994f78
Dmitry Doroshev <r.schweppes@ya.ru>
2019-10-12 18:15:52 +0300

    struct

--- array.c
+++ array.c
@@ -3,77 +3,95 @@
 
 #include "array.h"
 
-const int SIZE = 1;
+const int SIZE = 10*1024*1024;
+
+void run() {
+    printf("Initing...\n");
+    array *ar = init_array(SIZE);
+    printf("Filling...\n");
+    for (int i = 0; i < ar->length; i++) {
+        set_value(ar, i, i);
+    }
+    printf("Freeing...\n");
+    free_array(ar);
+    printf("done\n");
+}
 
 int main(void)
 {
-    int **ar = init_array(SIZE);
-    set_value(ar, 12, 111);
-    int *val = get_value(ar, 12);
-    printf("%d\n", *val);
-    // printf("Fill...");
-    // for (int i = 0; i < SIZE; i++) {
-    //     set_value(ar, i, i);
-    // }
-    // printf("%ld\n", sizeof(ar));
-    // printf("done\n");
-    // dump_array(ar);
-    // int *val = get_value(ar, 2);
-    // printf("%d\n", *val);
-    // free_array(ar);
-    // printf("done\n");
+    for (int i = 0; i < 10; i++) {
+        run();
+        printf("iter %d", i);
+        getchar();
+    }
 }
 
-int **init_array(int size)
+array *init_array(int length)
 {
-    int **ar;
-    if ((ar = malloc(size * sizeof(int*))) == NULL) {
+    array *ar = malloc(sizeof(array));
+    if (ar == NULL) {
         return NULL;
     }
 
-    for (int i = 0; i < size; i++) {
-        ar[i] = NULL;
+    if ((ar->values = malloc(length * sizeof(int*))) == NULL) {
+        free(ar);
+        return NULL;
     }
+    for (int i = 0; i < length; i++) {
+        ar->values[i] = NULL;
+    }
+
+    ar->length = length;
     return ar;
 }
 
-void dump_array(int **ar)
+void dump_array(array *ar)
 {
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < ar->length; i++) {
         int val = 0;
-        if (ar[i] != NULL) {
-            val = *ar[i];
+        if (ar->values[i] != NULL) {
+            val = *ar->values[i];
         }
-        printf("%d: %p -> %d\n", i, ar[i], val);
+        printf("%d: %p -> %d\n", i, ar->values[i], val);
     }
 }
 
-void free_array(int **ar)
+void free_array(array *ar)
 {
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < ar->length; i++) {
         delete_value(ar, i);
     }
+    free(ar->values);
     free(ar);
 }
 
-int set_value(int **ar, int index, int val)
+int set_value(array *ar, int index, int val)
 {
+    if (index >= ar->length) {
+        return -1;
+    }
     int *v = malloc(sizeof(int));
     *v = val;
-    ar[index] = v;
+    ar->values[index] = v;
 
     return 0;
 }
 
-int *get_value(int **ar, int index)
+int *get_value(array *ar, int index)
 {
-    return ar[index];
+    if (index >= ar->length) {
+        return NULL;
+    }
+    return ar->values[index];
 }
 
-int delete_value(int **ar, int index)
+int delete_value(array *ar, int index)
 {
-    free(ar[index]);
-    ar[index] = NULL;
+    if (index >= ar->length) {
+        return -1;
+    }
+    free(ar->values[index]);
+    ar->values[index] = NULL;
 
     return 0;
 }
--- array.h
+++ array.h
@@ -1,9 +1,14 @@
 #pragma once
 
-int **init_array(int size);
-void free_array(int **ar);
-void dump_array(int **ar);
+typedef struct {
+    int length;
+    int **values;
+} array;
 
-int set_value(int **ar, int index, int val);
-int *get_value(int **ar, int index);
-int delete_value(int **ar, int index);
+array *init_array(int length);
+void free_array(array *ar);
+void dump_array(array *ar);
+
+int set_value(array *ar, int index, int val);
+int *get_value(array *ar, int index);
+int delete_value(array *ar, int index);
