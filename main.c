423e1613ed9745a52690070582d712f7e2c13cd7
Dmitry Doroshev <r.schweppes@ya.ru>
2019-10-12 20:40:39 +0300

    Add main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "hashmap.h"
+
+int main(void)
+{
+    int i;
+    hashmap *hm = hashmap_init(HASHMAP_BASE_SIZE);
+    int keys[HASHMAP_BASE_SIZE] = {
+        156, 167, 234, 343, 75, 884, 500, 590,
+        /* 0   1    2    3   4    5    6    7 */
+    };
+
+    for (i = 0; i < HASHMAP_BASE_SIZE; i++) {
+        hashmap_set_value(hm, keys[i], &i);
+    }
+
+    for (i = 0; i < HASHMAP_BASE_SIZE; i++) {
+        int *pval = hashmap_get_value(hm, keys[i]);
+        if (pval == NULL) {
+            printf("[%d]: NOT EXIST\n", keys[i]);
+        } else {
+            printf("[%d]: %d\n", keys[i], *pval);
+        }
+    }
+    printf("\n");
+    hashmap_dump(hm);
+
+    hashmap_free(hm);
+    return 0;
+}
